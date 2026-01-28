2f5c64838ee544b73980d9a296e98fb34e035328
j0zko <jozefgunar952@gmail.com>
2026-01-28 22:19:37 +0100

    implemented POST

--- JavaHttpServer.java
+++ JavaHttpServer.java
@@ -21,24 +21,48 @@ public class JavaHttpServer {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
       try {
-        String query = exchange.getRequestURI().getQuery();
+        String method = exchange.getRequestMethod();
         String name = "Guest";
         String response;
-        if ("GET".equals(method)) {
-        String query = exchange.getRequestURI().getQuery();
-        if (query != null %% query.contains("name=")) {
-          name = query.split("=")[1];
-        }
-        response = "Hello, " + name + "!";
-        } else if ("POST".equals(method)) {
 
-        String response = "Hello, " + name + "!";
+        if ("GET".equals(method)) {
+          // Handle GET with query param
+          String query = exchange.getRequestURI().getQuery();
+          if (query != null && query.contains("name=")) {
+            name = query.split("=")[1];
+          }
+          response = "Hello, " + name + "! (via GET)";
+        } else if ("POST".equals(method)) {
+          // Handle POST by reading body (assume simple "name=xxx" format)
+          InputStream is = exchange.getRequestBody();
+          BufferedReader br = new BufferedReader(new InputStreamReader(is));
+          String body = br.readLine(); // Read single line for simplicity
+          if (body != null && body.contains("name=")) {
+            name = body.split("=")[1];
+          }
+          response = "Hello, " + name + "! (via POST)";
+        } else {
+          // Unsupported method
+          response = "Method not allowed.";
+          exchange.sendResponseHeaders(405, response.getBytes().length);
+          OutputStream os = exchange.getResponseBody();
+          os.write(response.getBytes());
+          os.close();
+          return;
+        }
+
+        // Send success response
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
 
       } catch (Exception e) {
         String errorResponse = "Internal server error.";
         exchange.sendResponseHeaders(500, errorResponse.getBytes().length);
-        exchange.getResponseBody().write(errorResponse.getBytes());
-        exchange.getResponseBody().close();
+        OutputStream os = exchange.getResponseBody();
+        os.write(errorResponse.getBytes());
+        os.close();
       }
     }
   }
