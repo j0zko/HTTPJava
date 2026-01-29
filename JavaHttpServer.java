505a44d74082159ae659312aa7e26d7df8591f08
j0zko <jozefgunar952@gmail.com>
2026-01-29 18:39:25 +0100

    feat:added parse query ,headers for POST check  and logging with concurrency
    parse query to handle multiple params and URL decoding
    Logging with  java util logger for err and startup
    Concurency fixed thread pool

--- JavaHttpServer.java
+++ JavaHttpServer.java
@@ -1,5 +1,3 @@
-import com.sun.net.httpserver.HttpServer;
-import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import java.io.OutputStream;
